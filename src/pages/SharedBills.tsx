394d6b205f29e7f9a99673513dbf4b87a86e1c9b
[dyad] <contatoalugueionline@gmail.com>
2026-05-11 09:06:16 -0300

    [dyad] Corrigindo erro de sintaxe em SharedBills - wrote 1 file(s)

--- src/pages/SharedBills.tsx
+++ src/pages/SharedBills.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { DashboardLayout } from '@/components/layout/DashboardLayout';
 import { Card, CardContent } from '@/components/ui/card';
