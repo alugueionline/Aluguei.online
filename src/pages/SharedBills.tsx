93dbc2922df4e56c0cb5c643d27f0cf1d83e3368
[dyad] <contatoalugueionline@gmail.com>
2026-05-11 09:03:13 -0300

    [dyad] Corrigi o erro de token inesperado na página de Contas Compartilhadas reescrevendo o arquivo com sintaxe JSX limpa. - wrote 1 file(s)

--- src/pages/SharedBills.tsx
+++ src/pages/SharedBills.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState } from 'react';
 import { DashboardLayout } from '@/components/layout/DashboardLayout';
 import { Card, CardContent } from '@/components/ui/card';
